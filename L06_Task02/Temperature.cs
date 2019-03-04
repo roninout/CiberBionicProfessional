8049d1fd98d121abc4e54c442d1715294b7466a0
roninout <roninorgua@gmail.com>
2019-03-04 23:26:52 +0300

    Add files via upload

---
+++ L06_Task02/Temperature.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L06_Task02
+{
+    public class Temperature
+    {
+        // конвертировать Цельсия в Фаренгейта
+        public string CelToFahrenheit(double temperature)
+        {
+            return $"{temperature} Цельсия =    {temperature * 1.80 + 32.00} Фаренгейта";
+        }
+
+        // конвертировать Цельсия в Кельвина
+        public string CelToKelvin(double temperature)
+        {
+            return $"{temperature} Цельсия =    {temperature + 273.15} Кельвина";
+        }
+
+        // конвертировать Фаренгейта в Кельвина
+        public string FahrenheitToKelvin(double temperature)
+        {
+            return $"{temperature} Фаренгейта = {(temperature - 32.00)/ 1.800 + 273.15} Кельвина";
+        }
+
+        // конвертировать Фаренгейта в Цельсия
+        public string FahrenheitToCel(double temperature)
+        {
+            return $"{temperature} Фаренгейта = {(temperature - 32.00) / 1.800} Цельсия";
+        }
+
+        // конвертировать Кельвина в Цельсия
+        public string KelvinToCel(double temperature)
+        {
+            return $"{temperature} Кельвина =   {temperature - 273.15} Цельсия";
+        }
+
+        // конвертировать Кельвина в Фаренгейта
+        public string KelvinToFahrenheit(double temperature)
+        {
+            return $"{temperature} Кельвина =   {(temperature - 273.15) * 1.8000 + 32.00} Фаренгейта";
+        }
+    }
+}
---
+++ L06_Task03/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L06_Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Assembly assembly = Assembly.Load("L06_Task02");
+                object instance = Activator.CreateInstance(assembly.GetType("L06_Task02.Temperature"));
+
+                // получаем информацию об Именах всех методов
+                Type t = assembly.GetType("L06_Task02.Temperature");
+                MethodInfo[] mi = t.GetMethods(BindingFlags.Instance
+                                             | BindingFlags.Static
+                                             | BindingFlags.Public
+                                             | BindingFlags.NonPublic
+                                             | BindingFlags.DeclaredOnly);
+
+                foreach (MethodInfo m in mi)
+                {
+                    Console.WriteLine(m.Invoke(instance, new object[] { 15.5 }));
+
+                }
+
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
