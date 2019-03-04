5178f69b9ae06db5c24e38512243d4db6fcdba69
ron_in <roninorgua@gmail.com>
2019-03-04 20:58:05 +0200

    Добавьте файлы проекта.

---
+++ L01_Task02/MonthCollection.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01_Task02
+{
+    class MonthCollection : IEnumerator, IEnumerable
+    {
+        int year;                                 // с каким годом будем организовывать месяцы
+        const int size = 12;
+        int[] months = new int[size];             // массив с месяцами
+        string[] nameMonths = new string[size];   // массив с названиями месяцев
+        int[] dayMonth = new int[size];           // массив с количеством дней в месяце
+
+        int position = -1;
+
+        // пользовательский конструктор
+        public MonthCollection(int year)
+        {
+            this.year = year;
+            InitMonthData();
+            Console.WriteLine($"Выбран год: {year}");
+        }
+
+        // инициализация массивов данными
+        private void InitMonthData()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                months[i] = i + 1;
+                nameMonths[i] = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(i + 1);
+                dayMonth[i] = DateTime.DaysInMonth(year, i + 1);
+            }
+        }
+
+
+        #region Реализация интерфейсов
+        public object Current
+        {
+            get
+            {
+                return string.Format($"№ - {months[position]} | name - {nameMonths[position]} | days - {dayMonth[position]}");
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            if (position < months.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        #endregion
+
+
+        public string MonthsByDay(int _day)
+        {
+            string result = "";
+            for (int i = 1; i < dayMonth.Length; i++)
+            {
+                if (dayMonth[i] == _day)
+                    result += $"№ - {months[i]} | name - {nameMonths[i]} | days - {dayMonth[i]} \n";
+            }
+            return result;
+        }
+
+        public string DaysByMonth(int _month)
+        {
+            string result = "";
+
+            for (int i = 1; i < months.Length; i++)
+            {
+                if (months[i] == _month)
+                {
+                    result += $"№ - {months[i]} | name - {nameMonths[i]} | days - {dayMonth[i]} \n";
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ L01_Task02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01_Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MonthCollection monthCollection = new MonthCollection(2056);
+
+            Console.WriteLine(new string('-', 50));
+            foreach (var item in monthCollection)
+                Console.WriteLine(item);
+
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine("Поиск месяцев по дням");
+            Console.WriteLine(monthCollection.MonthsByDay(31));
+
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine("Выводим количество дней в месяце");
+            Console.WriteLine(monthCollection.DaysByMonth(7));
+
+            Console.ReadKey();
+        }
+    }
+}
